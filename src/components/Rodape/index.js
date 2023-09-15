acf75f4e11be8ac858ffce2a30c666bc39b8da57
Pedrohdsg <pedrohdsg@gmail.com>
2023-09-15 18:41:44 -0300

    Continuando

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css'
+
+function Rodape (){
+    return (
+        <footer className={styles.rodape}>
+            <h2>Pedro Gondim 2023.</h2>
+        </footer>
+    )
+}
+
+export default Rodape;
